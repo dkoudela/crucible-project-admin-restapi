785a9242c9d270023312acf706283c3c1cae158a
David Koudela <dkoudela@volny.cz>
2016-10-13 04:48:21 -0700

    Preparation for Integration Tests.

---
+++ src/test/java/it/com/davidkoudela/crucible/RepositoryListRestApiWiredTest.java
@@ -0,0 +1,29 @@
+package it.com.davidkoudela.crucible;
+
+import com.davidkoudela.crucible.model.RepositoryAdminModel;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import com.atlassian.plugins.osgi.test.AtlassianPluginsTestRunner;
+import com.atlassian.sal.api.ApplicationProperties;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(AtlassianPluginsTestRunner.class)
+public class RepositoryListRestApiWiredTest
+{
+    private final ApplicationProperties applicationProperties;
+    private final RepositoryAdminModel repositoryAdminModel;
+
+    public RepositoryListRestApiWiredTest(ApplicationProperties applicationProperties, RepositoryAdminModel repositoryAdminModel)
+    {
+        this.applicationProperties = applicationProperties;
+        this.repositoryAdminModel = repositoryAdminModel;
+    }
+
+    @Test
+    public void testMyName()
+    {
+        assertEquals("!!! GOTCHA !!!!", true, false);
+//        assertEquals("names do not match!", "myComponent:" + applicationProperties.getDisplayName(), advancedLdapUserManager.getClass().toString());
+    }
+}
